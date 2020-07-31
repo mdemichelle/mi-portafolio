3970715527fff5327b803114f6d393944f22e163
michellemb306 <michellemirandab@gmail.com>
2020-07-31 17:45:25 -0400

    Agregando carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,24 @@ window.scroll({
        id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { 
            js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } 
     })()
+
+    $(function(){
+
+        $("a").click(function(event){
+          if (this.hash !== "") {
+            event.preventDefault();
+      
+            var gato = this.hash;
+      
+            $("html, body").animate({
+              scrollTop: $(gato).offset().top
+            }, 800, function(){
+              window.location.hash = gato;
+            });
+          }
+        });
+      
+        $('[data-toggle="popover"]').popover();
+      
+      });
+      
