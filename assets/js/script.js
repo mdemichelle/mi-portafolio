afc98021224892b96d8bcdd9826c23cdfaedbbf6
michellemb306 <michellemirandab@gmail.com>
2020-08-01 21:15:25 -0400

    Modificaciones y detalles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,20 @@
-//Smooth Scroll
-window.scroll({
-    top: 100, 
-    left: 0, 
-    behavior: 'smooth'
+
+$(function(){
+
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function(){
+        window.location.hash = gato;
+      });
+    }
   });
 
-//TypeForm
-  (function() {
-       var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, 
-       id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { 
-           js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } 
-    })()
+  $('[data-toggle="popover"]').popover();
 
-    $(function(){
-
-        $("a").click(function(event){
-          if (this.hash !== "") {
-            event.preventDefault();
-      
-            var gato = this.hash;
-      
-            $("html, body").animate({
-              scrollTop: $(gato).offset().top
-            }, 800, function(){
-              window.location.hash = gato;
-            });
-          }
-        });
-      
-        $('[data-toggle="popover"]').popover();
-      
-      });
-      
+});
